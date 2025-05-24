18559f562f562f7b301fd771585a4d68cb5d26ca
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-05-24 21:00:31 +0100

    Created - GPU project: Initilialization

--- matrix-benchmark-gpu/main.cu
+++ matrix-benchmark-gpu/main.cu
@@ -1,6 +1,30 @@
-#include <iostream>
+#include <stdio.h>
+
+__global__ void add(const int *a, const int *b, int *c) {
+  *c += *a + *b;
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  int a, b, c;
+  int *a_ptr, *b_ptr, *c_ptr;
+  const int size = sizeof(int);
+
+  cudaMalloc((void **) &a_ptr, size);
+  cudaMalloc((void **) &b_ptr, size);
+  cudaMalloc((void **) &c_ptr, size);
+
+  a = 2;
+  b = 3;
+  c = 5;
+
+  cudaMemcpy(a_ptr, &a, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(b_ptr, &b, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(c_ptr, &c, size, cudaMemcpyHostToDevice);
+
+  add<<<1,1>>>(a_ptr, b_ptr, c_ptr);
+
+  cudaMemcpy(&c, c_ptr, size, cudaMemcpyDeviceToHost);
+
+  printf("The sum is: %d\n", c);
   return 0;
 }
