dc948cb7db26f6288dde944f5f1ae522dac63536
TheTatsujin <luis.martin119@alu.ulpgc.es>
2025-05-25 22:35:44 +0100

    Updated - Cluster project: CMake now compiles with mpicc

--- matrix-benchmark-cluster/main.c
+++ matrix-benchmark-cluster/main.c
@@ -1,6 +1,11 @@
+#include <mpi.h>
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello, World!\n");
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  printf("Hello from rank %d\n", rank);
+  MPI_Finalize();
   return 0;
 }
